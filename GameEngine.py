4fc749b4dcefba52de4f09ad7bb08626af89f544
dudiiiiiiii <dudekb@student.agh.edu.pl>
2022-04-03 19:18:58 +0200

    map_boundries + cross walking

--- GameEngine.py
+++ GameEngine.py
@@ -11,9 +11,9 @@ class GameEngine:
         pygame.display.set_caption("Top-Down Shooter")
 
     def run(self):
-
+        w, h = self.screen.get_size()
         running = True
-        game_map = Map()
+        game_map = Map(w, h)
         flag_left = False
         flag_right = False
         flag_up = False
@@ -49,7 +49,8 @@ class GameEngine:
 
             game_map.move_hero(flag_up, flag_down, flag_left, flag_right)
             x, y = game_map.get_camera_position()
-            self.screen.blit(game_map.image, pygame.math.Vector2(0, 0), pygame.Rect(y, x, 800, 600))
+            self.screen.blit(game_map.image, pygame.math.Vector2(0, 0), pygame.Rect(x, y, 800, 600))
             self.screen.blit(game_map.hero.image, game_map.hero.screen_position)
+            print(game_map.hero.map_position)
 
             pygame.display.update()
--- Map.py
+++ Map.py
@@ -1,12 +1,15 @@
 import pygame
+import math
 
 from Hero import Hero
 
 
 class Map:
-    def __init__(self):
+    def __init__(self, w, h):
         self.width = 2000
         self.height = 2000
+        self.window_width = w
+        self.window_height = h
         self.image = pygame.image.load("assets/background.png")
         self.hero = Hero(pygame.math.Vector2(999, 999), pygame.math.Vector2(400, 300))
 
@@ -15,20 +18,38 @@ class Map:
         if (flag_down and flag_up) or not (flag_down or flag_up):
             change_y = 0
         elif flag_down:
-            change_y = 0.1
+            change_y = 0.5
         else:
-            change_y = -0.1
+            change_y = -0.5
         if (flag_left and flag_right) or not (flag_left or flag_right):
             change_x = 0
         elif flag_left:
-            change_x = -0.1
+            change_x = -0.5
         else:
-            change_x = 0.1
+            change_x = 0.5
 
-        self.hero.map_position.x += change_y
-        self.hero.map_position.y += change_x
+        if change_x and change_y and self.can_move_to_y(self.hero.map_position.y + change_y):
+            if self.can_move_to_x(self.hero.map_position.x + change_x) :
+                change_x /= math.sqrt(2)
+                change_y /= math.sqrt(2)
+
+        if self.can_move_to_x(self.hero.map_position.x + change_x):
+            self.hero.map_position.x += change_x
+        if self.can_move_to_y(self.hero.map_position.y + change_y):
+            self.hero.map_position.y += change_y
 
     def get_camera_position(self):
         camera_x = self.hero.map_position.x - self.hero.screen_position.x
         camera_y = self.hero.map_position.y - self.hero.screen_position.y
         return camera_x, camera_y
+
+    def can_move_to_x(self, x):
+        if x > self.window_width/2 and x < self.width - self.window_width/2:
+
+            return True
+        return False
+
+    def can_move_to_y(self, y):
+        if y > self.window_height / 2  and y < self.height - self.window_height/2:
+            return True
+        return False
