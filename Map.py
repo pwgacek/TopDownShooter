2e7d8ebbfcc692bd2c7dc6fcfeb5ff1a31e873b3
pwgacek <pawelgacek.pl@gmail.com>
2022-04-10 00:22:41 +0200

    dodano potwory,naprowadzanie, otaczanie

--- Map.py
+++ Map.py
@@ -2,7 +2,9 @@ import pygame
 import math
 
 from Hero import Hero
+from Monster import Monster
 from pygame.math import Vector2
+from random import randint
 
 
 class Map:
@@ -12,9 +14,9 @@ class Map:
         self.__screen_size = screen_size
         self.__image = pygame.image.load("assets/background2.jpg")
         self.__hero = Hero(Vector2(self.__width / 2, self.__height / 2), self.__screen_size)
+        self.__monsters = list()
 
-    def move_hero(self, move_direction_flags, fps):
-        move_speed = 0.3*fps/3
+    def move_hero(self, move_direction_flags, move_speed):
 
         if (move_direction_flags["down"] and move_direction_flags["up"]) or \
                 not (move_direction_flags["down"] or move_direction_flags["up"]):
@@ -48,11 +50,58 @@ class Map:
 
         self.__hero.set_angle()
 
+    def move_monsters(self, move_speed):
+
+        half_image_size = self.__hero.get_image().get_size()[1] / 2
+        for monster in self.__monsters:
+            if math.dist(monster.get_map_position(), self.__hero.get_map_position()) > half_image_size:
+                x = monster.get_map_position().x + monster.get_unit_vector().x * move_speed
+                y = monster.get_map_position().y + monster.get_unit_vector().y * move_speed
+                xl = monster.get_map_position().x + monster.get_unit_vector(
+                    (monster.get_angle() - 60) % 360).x * move_speed
+                yl = monster.get_map_position().y + monster.get_unit_vector(
+                    (monster.get_angle() - 60) % 360).y * move_speed
+                xr = monster.get_map_position().x + monster.get_unit_vector(
+                    (monster.get_angle() + 60) % 360).x * move_speed
+                yr = monster.get_map_position().y + monster.get_unit_vector(
+                    (monster.get_angle() + 60) % 360).y * move_speed
+                can_move = True
+                can_move_after_left_rotate = True
+                can_move_after_right_rotate = True
+
+                for monster2 in self.__monsters:
+                    distance = math.dist(monster.get_map_position(), monster2.get_map_position())
+
+                    if monster2 != monster and distance < half_image_size:
+                        if distance >= math.dist(Vector2(x, y), monster2.get_map_position()):
+                            can_move = False
+                        if distance >= math.dist(Vector2(xl, yl), monster2.get_map_position()):
+                            can_move_after_left_rotate = False
+                        if distance >= math.dist(Vector2(xr, yr), monster2.get_map_position()):
+                            can_move_after_right_rotate = False
+
+                        if not (can_move or can_move_after_right_rotate or can_move_after_left_rotate):
+                            break
+
+                monster.set_angle(self.__hero.get_map_position())
+                if can_move:
+                    monster.get_map_position().x = x
+                    monster.get_map_position().y = y
+
+                elif can_move_after_left_rotate:
+                    monster.get_map_position().x = xl
+                    monster.get_map_position().y = yl
+                    monster.set_angle(self.__hero.get_map_position())
+                elif can_move_after_right_rotate:
+                    monster.get_map_position().x = xr
+                    monster.get_map_position().y = yr
+                    monster.set_angle(self.__hero.get_map_position(),)
+
     def get_camera_position(self):
         """get position of rectangle (cut out of background image) which will be shown on the screen"""
         camera_x = self.__hero.get_map_position().x - self.__hero.get_screen_position().x
         camera_y = self.__hero.get_map_position().y - self.__hero.get_screen_position().y
-        return camera_x, camera_y
+        return Vector2(camera_x, camera_y)
 
     def __can_move_to_x(self, x):
         return self.__screen_size.x / 2 < x < self.__width - self.__screen_size.x / 2
@@ -65,3 +114,15 @@ class Map:
 
     def get_hero(self):
         return self.__hero
+
+    def get_monsters(self):
+        return self.__monsters
+
+    def add_monster(self):
+        x = randint(0, self.__width)
+        y = randint(0, self.__height)
+        self.__monsters.append(Monster(Vector2(x, y)))
+        print(x, y)
+
+    def __remove_monster(self, monster):
+        self.__monsters.remove(monster)
---
+++ Monster.py
@@ -0,0 +1,59 @@
+import math
+
+import pygame
+from pygame.math import Vector2
+
+
+class Monster:
+    def __init__(self, map_position):
+        self.__map_position = map_position
+        self.__angle = 0
+        self.__image = pygame.image.load("assets/monster1.png");
+
+    def __eq__(self, other):
+        return self is other
+
+    def get_map_position(self):
+        return self.__map_position
+
+    def get_angle(self):
+        return self.__angle
+
+    def get_image(self):
+        return self.__image
+
+    def get_unit_vector(self, a=None):
+        """returns angle converted to unit vector"""
+        if a is None:
+            a = self.__angle
+        x = math.sin(math.radians(a - 180))
+        y = math.cos(math.radians(a - 180))
+
+        return Vector2(x, y)
+
+    def get_rotated_image(self):
+        """ returns rotated  image while keeping its center and size"""
+
+        orig_rect = self.__image.get_rect()
+        rot_image = pygame.transform.rotate(self.__image, self.__angle)
+        rot_rect = orig_rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        rot_image = rot_image.subsurface(rot_rect).copy()
+        return rot_image
+
+    def get_screen_position(self, camera_position):
+        return Vector2(self.__map_position.x - camera_position.x, self.__map_position.y - camera_position.y)
+
+    def set_angle(self, hero_map_position):
+        """sets value of self.__angle in accordance with hero position"""
+
+        hero_center = Vector2(hero_map_position.x + self.__image.get_size()[0] / 2,
+                              hero_map_position.y + self.__image.get_size()[1] / 2)
+        my_center = Vector2(self.__map_position.x + self.__image.get_size()[0] / 2,
+                            self.__map_position.y + self.__image.get_size()[1] / 2)
+        distance = math.dist(hero_center, my_center)
+        if distance > 1:
+            self.__angle = math.degrees(math.acos((my_center.y - hero_center.y) / distance))
+            if my_center[0] < hero_center[0]:
+                self.__angle = 360.0 - self.__angle
+
