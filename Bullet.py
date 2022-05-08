6cd626f75404aa93137def06daf5a42e94291ab9
pwgacek <pawelgacek.pl@gmail.com>
2022-05-08 19:12:00 +0200

    mala zmiana w pocisku

--- Bullet.py
+++ Bullet.py
@@ -4,15 +4,16 @@ import math
 
 
 class Bullet:
-
     __image = pygame.image.load("assets/bullet.png")
 
     def __init__(self, map_position, angle, hero_size):
         self.__angle = angle
-        #self.__image = pygame.transform.rotate(pygame.image.load("assets/bullet.png"), angle - 90)
+        # self.__image = pygame.transform.rotate(pygame.image.load("assets/bullet.png"), angle - 90)
         self.__map_position = Vector2(
-            map_position.x + hero_size[0]/2 - self.__image.get_size()[0]/2 + math.cos(math.radians(angle-14)) * 50,
-            map_position.y + hero_size[1]/2 - self.__image.get_size()[1]/2 - math.sin(math.radians(angle-14)) * 50)
+            map_position.x + hero_size[0] / 2 - self.__image.get_size()[0] / 2 + math.cos(
+                math.radians(angle - 14)) * 50,
+            map_position.y + hero_size[1] / 2 - self.__image.get_size()[1] / 2 - math.sin(
+                math.radians(angle - 14)) * 50)
 
     def move(self):
         """ moves current bullet on  map"""
@@ -31,6 +32,7 @@ class Bullet:
 
     def get_angle(self):
         return self.__angle
+
     @classmethod
     def get_image(cls):
         return cls.__image
